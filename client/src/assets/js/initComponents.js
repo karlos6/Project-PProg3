803f34716755cc156c1bd04167913ecd66059781
karlos6 <Carlos.Keller.M@gmail.com>
2020-01-20 14:50:31 -0500

    creacion master-page & archivos en el assets css, js y images

---
+++ client/src/assets/js/initComponents.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+  M.AutoInit();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
