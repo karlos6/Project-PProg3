5c1aef0b5068109d6e0e4b0d97cb4f7cf4661a9f
karlos6 <Carlos.Keller.M@gmail.com>
2020-01-25 17:32:45 -0500

    creacion validaciones register

--- client/src/app/modules/security/register/register.component.ts
+++ client/src/app/modules/security/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  
+  RValidation: FormGroup;
+
+  constructor(private rb: FormBuilder) { }
 
   ngOnInit() {
+    this.RValidationBuilder();
+  }
+
+  RValidationBuilder() {
+    this.RValidation = this.rb.group({
+
+      firstname: ['', [Validators.required, Validators.maxLength(12)]],
+      lastname: ['', [Validators.required, Validators.maxLength(12)]],
+      username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20), Validators.email]],
+      password: ['', [Validators.required, Validators.maxLength(15), Validators.minLength(4)]]
+
+    });
+  }
+
+  get rr(){
+
+    return this.RValidation.controls;
+
   }
 
 }
